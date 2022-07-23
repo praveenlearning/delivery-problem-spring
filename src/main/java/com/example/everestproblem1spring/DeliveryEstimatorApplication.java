958ca92a217dca57bee81cead1ac36efd2194c38
praveen <praveen.bathula@thoughtworks.com>
2022-07-23 12:45:10 +0530

    Initialise spring project

---
+++ src/main/java/com/example/everestproblem1spring/DeliveryEstimatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.everestproblem1spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryEstimatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryEstimatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/everestproblem1spring/EverestProblem1SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.everestproblem1spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverestProblem1SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
